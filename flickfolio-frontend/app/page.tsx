655492e0ca604af3b25a709554c88001f62744fc
Don Roy Chacko <donisepic30@gmail.com>
2024-08-20 09:15:52 +0530

    Homepage layout done, only content left to add

--- flickfolio-frontend/app/page.tsx
+++ flickfolio-frontend/app/page.tsx
@@ -7,21 +7,32 @@ export default function Home() {
   return (
     <main className="">
       <div className="max-h-screen max-w-screen relative h-screen object-none">
-        <div className="-z-100 absolute top-0 left-0 object-none max-h-full h-full">
+        <div className="-z-100 absolute top-0 left-0 object-none max-h-screen h-screen">
           <EmblaCarousel className="object-none" />
         </div>
         <div className="z-100 relative max-h-full h-full flex flex-col">
           <div className="pt-8 pl-12">
             <Logo/>
           </div>
-          <div className="text-white flex flex-col items-center flex-1 h-full mt">
-            <h1 className="text-6xl">Your Movie Lists, Your Way.</h1>
-            <h2 className="text-xl">Curate Your Perfect Movie Lists, Share Your Favorites, and Discover New Must-Sees</h2>
-            <Button className="mt-5">Get Started</Button>
+          <div className="text-white flex items-center h-full mt relative">
+            <div className="absolute flex flex-col items-center top-1/4 left-0 right-0">
+                <h1 className="text-6xl font-bold">Your Movie Lists, Your Way.</h1>
+                <h2 className="text-xl font-extralight mt-5">Curate Your Perfect Movie Lists, Share Your Favorites, and Discover New Must-Sees</h2>
+                <Button className="mt-36 bg-secondaryBackground rounded-full px-10 py-9"><p className="text-2xl">Get Started</p></Button>
+            </div>
+            
+            
           </div>
-          
         </div>
-        
+      </div>
+      <div className="h-screen z-100 relative bg-secondaryBackground">
+
+      </div>
+      <div className="h-screen z-100 relative bg-primaryBackground">
+
+      </div>
+      <div className=" h-96 bg-secondaryBackground relative z-100">
+      
       </div>
       
     </main>
--- flickfolio-frontend/tailwind.config.ts
+++ flickfolio-frontend/tailwind.config.ts
@@ -19,6 +19,8 @@ const config = {
     },
     extend: {
       colors: {
+        secondaryBackground: "#F55B4B",
+        primaryBackground: "#0D0D14",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
