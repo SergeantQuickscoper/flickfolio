23519410ff02ce0e3d8b000e1e5badaac5d3c2ed
Don Roy Chacko <donisepic30@gmail.com>
2024-08-27 09:29:52 +0530

    Search Button Icon added

--- flickfolio-frontend/components/ui/input.tsx
+++ flickfolio-frontend/components/ui/input.tsx
@@ -8,15 +8,19 @@ export interface InputProps
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
     return (
+      <div className="flex flex-row bg-background rounded-full focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2">
       <input
         type={type}
+        placeholder="Search"
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-6 py-6 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-10 w-full rounded-md t bg-background px-6 py-6 text-sm ring-offset-background focus:outline-none file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
         {...props}
       />
+      <img src="SearchIcon.svg"></img>
+      </div>
     )
   }
 )
