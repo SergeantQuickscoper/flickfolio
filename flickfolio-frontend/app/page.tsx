5dfaaa9a6939b52ebd96c5a4398d949eb6f225a5
Don Roy Chacko <donisepic30@gmail.com>
2025-02-04 22:13:14 +0530

    login screen layout

--- flickfolio-frontend/app/page.tsx
+++ flickfolio-frontend/app/page.tsx
@@ -23,14 +23,15 @@ export default async function Home() {
           <EmblaCarousel className="object-none" imgArray={slideshowArray}/>
         </div>
         <div className="z-100 relative max-h-full h-full flex flex-col">
-          <div className="">
+          <div className="flex flex-row justify-between">
             <Logo/>
+            <Button className=" bg-secondaryBackground rounded-full px-8 py-7 mt-9 mr-8"><Link href="/toprated"> <p className="text-2xl">Login</p></Link></Button>
           </div>
           <div className="text-white flex items-center h-full mt relative">
             <div className="absolute flex flex-col items-center top-1/4 left-0 right-0">
                 <h1 className="text-6xl font-bold">Your Movie Lists, Your Way.</h1>
                 <h2 className="text-xl font-extralight mt-5">Curate Your Perfect Movie Lists, Share Your Favorites, and Discover New Must-Sees</h2>
-                
+
                     <Button className="mt-36 bg-secondaryBackground rounded-full px-10 py-9"><Link href="/toprated"> <p className="text-2xl">Get Started</p></Link></Button>
 
             </div>
